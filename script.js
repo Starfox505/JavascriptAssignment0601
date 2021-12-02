a4f13ce8c4a91a7ab7c7666a20755ec4cafb3c31
Brendan Drewry <superbrendan715@yahoo.com>
2021-12-01 20:34:33 -0800

    Update script.js
    added comments

--- script.js
+++ script.js
@@ -17,15 +17,23 @@ function primitiveMultiply(a, b) {
 }
 
 function reliableMultiply(a, b) {
+  //This calls a new function reliable Multiply with values a and b, similar to primitive multiply.
   while (true) {
+    //This while statement says that while the statement is true than proceed.
     try {
+      //This is a try statement for the reliable multiply. This is the line of code we are testing.
       return primitiveMultiply(a, b);
+      //This means it will return primitive multiply's values of a and b.
     } catch (exception) {
+      //This will catch the errors and any exceptions.
       if (!(exception instanceof MultiplicatorUnitFailure)) {
+        //This if statement starts with an exclamation point, which means it negates the exception of the instance of Multiplactor Unit Failure from showing up in the console log.
         throw exception;
+        //This means it will throw the exception if the line of code does not come back with the correct or try statement.
       }
     }
   }
 }
 
 console.log(reliableMultiply(8, 8));
+//This logs the answer, and gives the a and b values both a numeric value of 8 for both the reliable multiply and primitive multiply, as the primitive multiply is wrapped in the reliable multiply.
